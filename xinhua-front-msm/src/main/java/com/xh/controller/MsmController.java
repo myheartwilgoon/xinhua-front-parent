606d0ab923f059e74399de033db2a46668ca284c
早晚会胖 <syw3612020@163.com>
2020-10-13 20:09:46 +0800

    cms

---
+++ xinhua-front-msm/src/main/java/com/xh/controller/MsmController.java
@@ -0,0 +1,18 @@
+package com.xh.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: xinhua-front-parent
+ * @description:
+ * @author: wys
+ * @create: 2020-10-12 20:13
+ */
+@RestController
+@RequestMapping("/edumsm/msm")
+@CrossOrigin
+public class MsmController {
+
+}
